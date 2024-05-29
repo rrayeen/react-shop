8cc918d04e2ec4916a62722613b050867983e514
rrayeen <ray682212@gmail.com>
2024-05-29 11:45:25 -1200

    configs changes

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  build: {
-    rollupOptions: {
-      external: ["react-redux"],
-    },
-  },
 });
